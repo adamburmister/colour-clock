493faf9f95f013300fa0b6665826b7877fd202da
Adam Burmister <adam.burmister@gmail.com>
2011-02-25 11:01:41 +0000

    First commit

---
+++ clock.js
@@ -0,0 +1,45 @@
+$(function() {
+  var time      = $("time"),
+			hour1     = $("#digit1"),
+		  hour2     = $("#digit2"),
+		  minute1   = $("#digit3"),
+		  minute2   = $("#digit4"),
+		  second1   = $("#digit5"),
+		  second2   = $("#digit6"),
+		  body      = $("body");
+
+  var setBackgroundColorToTime = function () {
+    var t = new Date(),
+		    h = t.getHours(),
+		    m = t.getMinutes(),
+		    s = t.getSeconds();
+		
+		time.attr("datetime", t.getTime());
+    hour1.html(Math.floor(h / 10));
+    hour2.html(h % 10);
+
+    minute1.html(Math.floor(m / 10));
+    minute2.html(m % 10);
+
+    second1.html(Math.floor(s / 10));
+    second2.html(s % 10);
+
+		body.css("background-color", "rgb(" + parseInt((h / 24) * 255) + "," +
+                        					parseInt((m / 60) * 255) + "," +
+                        					parseInt((s / 60) * 255) + ")")
+  };
+
+  setBackgroundColorToTime();
+
+  window.setInterval(setBackgroundColorToTime, 1000);
+
+	DEFAULT_FONT_FACE = "'Josefin Slab', Arial, sans-serif";
+	var fontList = $("nav ul");
+	fontList.delegate("li", "click", function() {
+		var face = $(this).text();
+		if(face == "Default") {
+			face = DEFAULT_FONT_FACE;
+		}
+		time.css("font-family", face);
+	});
+});
