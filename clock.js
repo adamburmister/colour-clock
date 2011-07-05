b4db9cdfe4216d896c3860482022785f77325063
Adam Burmister <adam.burmister@gmail.com>
2011-07-05 14:16:27 +0100

    Tweaking display of the font menu

--- clock.js
+++ clock.js
@@ -1,20 +1,20 @@
 $(function() {
   var time      = $("time"),
-			hour1     = $("#digit1"),
-		  hour2     = $("#digit2"),
-		  minute1   = $("#digit3"),
-		  minute2   = $("#digit4"),
-		  second1   = $("#digit5"),
-		  second2   = $("#digit6"),
-		  body      = $("body");
+      hour1     = $("#digit1"),
+      hour2     = $("#digit2"),
+      minute1   = $("#digit3"),
+      minute2   = $("#digit4"),
+      second1   = $("#digit5"),
+      second2   = $("#digit6"),
+      body      = $("body");
 
   var setBackgroundColorToTime = function () {
     var t = new Date(),
-		    h = t.getHours(),
-		    m = t.getMinutes(),
-		    s = t.getSeconds();
-		
-		time.attr("datetime", t.getTime());
+        h = t.getHours(),
+        m = t.getMinutes(),
+        s = t.getSeconds();
+    
+    time.attr("datetime", t.getTime());
     hour1.html(Math.floor(h / 10));
     hour2.html(h % 10);
 
@@ -24,25 +24,28 @@ $(function() {
     second1.html(Math.floor(s / 10));
     second2.html(s % 10);
 
-		body.css("background-color", "rgb(" + parseInt((h / 24) * 255) + "," +
-                        					parseInt((m / 60) * 255) + "," +
-                        					parseInt((s / 60) * 255) + ")")
+    body.css("background-color", "rgb(" + parseInt((h / 24) * 255) + "," +
+                                  parseInt((m / 60) * 255) + "," +
+                                  parseInt((s / 60) * 255) + ")")
   };
 
   setBackgroundColorToTime();
 
-	// Start the clock running
-	var runClock = function(){ window.setInterval(setBackgroundColorToTime, 1000); }
-	// Sync clock to sys clock
+  // Start the clock running
+  var runClock = function(){ window.setInterval(setBackgroundColorToTime, 1000); }
+  // Sync clock to sys clock
   window.setTimeout(runClock, 999-(new Date()).getMilliseconds());
 
-	DEFAULT_FONT_FACE = "'Permanent Marker', Arial, sans-serif";
-	var fontList = $("nav ul");
-	fontList.delegate("li", "click", function() {
-		var face = $(this).text();
-		if(face == "Default") {
-			face = DEFAULT_FONT_FACE;
-		}
-		time.css("font-family", face);
-	});
+  DEFAULT_FONT_FACE = "'Permanent Marker', Arial, sans-serif";
+  var fontList = $("nav ul");
+  fontList.delegate("li", "click", function() {
+    var face = $(this).text();
+    if(face == "Default") {
+      face = DEFAULT_FONT_FACE;
+    }
+    time.css("font-family", face);
+    
+    $(this).parent().find(".selected").removeClass("selected");
+    $(this).addClass("selected");
+  });
 });
